d5031dcc3f0a39f674b5aa43df42dc7712fbbfef
jlsigmon <32619336+jlsigmon@users.noreply.github.com>
2021-04-26 17:52:52 -0400

    Menu Options Start

--- NinjaMod/CustomGameOptions.cs
+++ NinjaMod/CustomGameOptions.cs
@@ -2,7 +2,7 @@ namespace NinjaMod
 {
     public static class CustomGameOptions
     {
-        public static bool showNinja=false;
+        public static bool ShowNinja=false;
         public static float NinjaKillCD=30.0f;
 
     }
