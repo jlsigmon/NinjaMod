c7f599ed56d43420a4fb32552b7e16511150d977
LV3Mods <78286687+LV3Mods@users.noreply.github.com>
2021-04-26 22:14:29 -0400

    Invisibility button added, (needs proper img)
    Added button

--- NinjaMod/Invisibility.cs
+++ NinjaMod/Invisibility.cs
@@ -3,7 +3,7 @@ using Reactor.Unstrip;
 using Reactor.Button;
 using UnityEngine;
 using HarmonyLib;
-namespace YourCoolMod
+namespace NinjaMod
 {
     [HarmonyPatch(typeof(HudManager), nameof(HudManager.Start))]
     public static class ExampleButton
@@ -18,12 +18,12 @@ namespace YourCoolMod
                     // Do cool stuff when the button is pressed
                 },
                 5f, // The cooldown for this button is five seconds
-                Properties.Resources.yournamehere, // change yournamehere to the name you set in step 2
+                Properties.Resources.demo_invis_button, // change yournamehere to the name you set in step 2
                 new Vector2(0.125f, 0.125f), // The position of the button, 1 unit is 100 pixels
                 () => 
                 {
                     // Who has access to the button? This allows alive crewmates to use the new button while the game is started
-                    return !PlayerControl.LocalPlayer.Data.IsDead && !PlayerControl.LocalPlayer.Data.IsImpostor && (AmongUsClient.Instance.GameState == InnerNetClient.GameStates.Started || AmongUsClient.Instance.GameMode == GameModes.FreePlay);
+                    return !PlayerControl.LocalPlayer.Data.IsDead && PlayerControlPatch.isNinja(PlayerControl.LocalPlayer) && (AmongUsClient.Instance.GameState == InnerNet.InnerNetClient.GameStates.Started || AmongUsClient.Instance.GameMode == GameModes.FreePlay);
                 },
                 __instance
             );
